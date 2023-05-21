d13ecb60235a709cff93044615051262bee92da9
Hey-Halo <89099540+Hey-Halo@users.noreply.github.com>
2023-05-21 15:23:25 +0200

    Add files via upload

--- Projekt_Filip_Marinkovic_314645/cartoonize.py
+++ Projekt_Filip_Marinkovic_314645/cartoonize.py
@@ -3,14 +3,23 @@ import numpy as np
 
 
 def cartoonize(image):
+    """A function that transforms an image into a cartoonized form"""
+
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     gray = cv.GaussianBlur(gray, (3, 3), 1)
     edges = cv.Laplacian(gray, -1, ksize=5)
     edges = 255 - edges
+
+    # cv.imshow("edges1", edges)
+
     ret, edges = cv.threshold(edges, 150, 255, cv.THRESH_BINARY)
+
+    # cv.imshow("edges2", edges)
+
     edgePreservingImage = cv.edgePreservingFilter(
         image, flags=2, sigma_s=150, sigma_r=0.6)
-    output = np.zeros(gray.shape)
-    output = cv.bitwise_and(edgePreservingImage,
-                            edgePreservingImage, mask=edges)
-    return output
+
+    # cv.imshow("edgePreservingImage", edgePreservingImage)
+
+    return cv.bitwise_and(edgePreservingImage,
+                          edgePreservingImage, mask=edges)
--- Projekt_Filip_Marinkovic_314645/main.py
+++ Projekt_Filip_Marinkovic_314645/main.py
@@ -1,30 +1,61 @@
 import cv2 as cv
 import cartoonize as ct
+import sys
+import os
 
-cap = cv.VideoCapture("VideoInput.mp4")
-height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
-width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
-fps = int(cap.get(cv.CAP_PROP_FPS))
 
-fourcc = cv.VideoWriter_fourcc(*'mp4v')
-output = cv.VideoWriter("VideoOutput.mp4", fourcc, fps, (width, height))
-cv.namedWindow("Video Player")
-cv.resizeWindow("Video Player", 1280, 720)
+def main():
+    sys.argv[0] = input("Enter the name of the file you wish to open: \n")
+    fileName = sys.argv[0]
+    fileExists = os.path.isfile(fileName)
 
-while (cap.isOpened()):
-    success, frame = cap.read()
-    if success:
-        frame = ct.cartoonize(frame)
-        cv.imshow("Video Player", frame)
-        output.write(frame)
-        quitButton = cv.waitKey(int(1/fps*1000)) & 0xFF == 27
-        closeButton = cv.getWindowProperty(
+    if fileExists == False:
+        print("""Error:File does not exist\n 
+        Check the spelling of the name, file extension or check if the file is in the right directory\n""")
+        return None
+
+    __cap = cv.Video__capture(fileName)
+
+    try:
+        __cap.isOpened()
+    except:
+        print("%s file couldn't be opened" % fileName)
+
+    __height = int(__cap.get(cv.__cap_PROP_FRAME_HEIGHT))
+    __width = int(__cap.get(cv.__cap_PROP_FRAME_WIDTH))
+    __fps = int(__cap.get(cv.__cap_PROP_FPS))
+    __fourcc = cv.VideoWriter_fourcc(*'mp4v')
+
+    try:
+        output = cv.VideoWriter("VideoOutput.mp4", __fourcc,
+                                __fps, (__width, __height))
+    except:
+        print("There is a problem with this directory or file extension")
+
+    cv.namedWindow("Video Player")
+    cv.resizeWindow("Video Player", 1280, 720)
+
+    while __cap.isOpened():
+        __success, __frame = __cap.read()
+        if __success:
+            __frame = ct.cartoonize(__frame)
+            cv.imshow("Video Player", __frame)
+            output.write(__frame)
+        else:
+            print("There was a problem with reading the video")
+            break
+
+        __quitKey = cv.waitKey(int(1/__fps*1000)) & 0xFF == 27
+        __closeButton = cv.getWindowProperty(
             "Video Player", cv.WND_PROP_VISIBLE) < 1
-    else:
-        break
-    if quitButton or closeButton:
-        break
 
-cap.release()
-output.release()
-cv.destroyAllWindows()
+        if __quitKey or __closeButton:
+            break
+
+    __cap.release()
+    output.release()
+    cv.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
